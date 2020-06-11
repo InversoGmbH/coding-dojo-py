f29708ea271fe475e9bd5d1203899b322643e9d1
Andreas Neumann <andreas.neumann@inverso.de>
2020-06-11 16:42:34 +0200

    Fehler in den Exception-Tests behoben und neue Assert-Funktion dabei gelernt.
    Restliche Unittests hinzugefügt

--- test/romanconvert/test_romanconvert.py
+++ test/romanconvert/test_romanconvert.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 from romanconvertkata.RomanConvertException import RomanConvertException
 from romanconvertkata.romanconvert import roman2decimal
 from unittest import TestCase
+import logging
 import math
 import random
 
@@ -56,45 +58,50 @@ def _create_roman(zahl: int) -> str:
 
 class TestRomanConvert(TestCase):
 
+    def assertEqualRoman(self, erwartet: int, eingabe: str):
+        self.assertEqual(erwartet, roman2decimal(eingabe),
+                         msg="roman2decimal(%s) != %i, erhaltener Rückgabewert: %i" %
+                             (eingabe, erwartet, roman2decimal(eingabe)))
+
     def test_einser(self):
         """i, ii und iii als Eingabeparameter"""
-        self.assertEqual(1, roman2decimal('i'))
-        self.assertEqual(2, roman2decimal('ii'))
-        self.assertEqual(3, roman2decimal('iii'))
+        self.assertEqualRoman(1, 'i')
+        self.assertEqualRoman(2, 'ii')
+        self.assertEqualRoman(3, 'iii')
 
     def test_fuenfer_potenzen(self):
         """v, l, d als Eingabeparameter"""
-        self.assertEqual(5, roman2decimal('v'))
-        self.assertEqual(50, roman2decimal('l'))
-        self.assertEqual(500, roman2decimal('d'))
+        self.assertEqualRoman(5, 'v')
+        self.assertEqualRoman(50, 'l')
+        self.assertEqualRoman(500, 'd')
 
     def test_zehner_potenzen(self):
         """x, xx, xxx, c, cc, ccc, m, mm und mmm als Eingabeparameter"""
-        self.assertEqual(10, roman2decimal('x'))
-        self.assertEqual(20, roman2decimal('xx'))
-        self.assertEqual(30, roman2decimal('xxx'))
-        self.assertEqual(100, roman2decimal('c'))
-        self.assertEqual(200, roman2decimal('cc'))
-        self.assertEqual(300, roman2decimal('ccc'))
-        self.assertEqual(1000, roman2decimal('m'))
-        self.assertEqual(2000, roman2decimal('mm'))
-        self.assertEqual(3000, roman2decimal('mmm'))
+        self.assertEqualRoman(10, 'x')
+        self.assertEqualRoman(20, 'xx')
+        self.assertEqualRoman(30, 'xxx')
+        self.assertEqualRoman(100, 'c')
+        self.assertEqualRoman(200, 'cc')
+        self.assertEqualRoman(300, 'ccc')
+        self.assertEqualRoman(1000, 'm')
+        self.assertEqualRoman(2000, 'mm')
+        self.assertEqualRoman(3000, 'mmm')
 
     def test_subtraktionsregel(self):
         """iv, ix, xl, xc, cd, cm als Eingabeparameter"""
-        self.assertEqual(4, roman2decimal('iv'))
-        self.assertEqual(9, roman2decimal('ix'))
-        self.assertEqual(40, roman2decimal('xl'))
-        self.assertEqual(90, roman2decimal('xc'))
-        self.assertEqual(400, roman2decimal('cd'))
-        self.assertEqual(900, roman2decimal('cm'))
+        self.assertEqualRoman(4, 'iv')
+        self.assertEqualRoman(9, 'ix')
+        self.assertEqualRoman(40, 'xl')
+        self.assertEqualRoman(90, 'xc')
+        self.assertEqualRoman(400, 'cd')
+        self.assertEqualRoman(900, 'cm')
 
     def test_kombination(self):
         """Kombiniere diverse römische Ziffern zu einer Zahl"""
-        self.assertEqual(8, roman2decimal('viii'))
-        self.assertEqual(42, roman2decimal('xlii'))
-        self.assertEqual(99, roman2decimal('xcix'))
-        self.assertEqual(2020, roman2decimal('mmxx'))
+        self.assertEqualRoman(8, 'viii')
+        self.assertEqualRoman(42, 'xlii')
+        self.assertEqualRoman(99, 'xcix')
+        self.assertEqualRoman(2020, 'mmxx')
 
     def test_random_kombination(self):
         """Randomzahlen zwischen 1 und 3999 prüfen"""
@@ -104,16 +111,16 @@ class TestRomanConvert(TestCase):
 
     def test_ulkige_zeichen(self):
         """Tests für ↁ und ↂ"""
-        self.assertEqual(5000, roman2decimal('ↁ'))
-        self.assertEqual(10000, roman2decimal('ↂ'))
-        self.assertEqual(4000, roman2decimal('mↁ'))
-        self.assertEqual(9000, roman2decimal('mↂ'))
-        self.assertEqual(39999, roman2decimal('ↂↂↂmↂcmxcix'))
+        self.assertEqualRoman(5000, 'ↁ')
+        self.assertEqualRoman(10000, 'ↂ')
+        self.assertEqualRoman(4000, 'mↁ')
+        self.assertEqualRoman(9000, 'mↂ')
+        self.assertEqualRoman(39999, 'ↂↂↂmↂcmxcix')
 
     def test_random_kombination_ulkige_zeichen(self):
         """Randomzahlen zwischen 4000 und 39999 prüfen"""
         for i in range(0, 100):
-            zahl = random.randint(i+4000, 39999)
+            zahl = random.randint(i + 4000, 39999)
             self.assertEqual(roman2decimal(_create_roman(zahl)), zahl)
 
     def test_unbekannte_zeichen_uebergeben(self):
@@ -123,6 +130,50 @@ class TestRomanConvert(TestCase):
         self._syntaktischer_fehler({'1': 'x'})
 
     def _syntaktischer_fehler(self, roman: str):
-        with self.assertRaises(RomanConvertException) as context:
+        with self.assertRaisesRegexp(RomanConvertException, 'Eingabe %s beinhaltet syntaktische Fehler' % roman):
             roman2decimal(roman)
-        self.assertTrue('Eingabe \'%s\' beinhaltet syntaktische Fehler' % roman in context.exception)
+
+    def test_semantischer_fehler(self):
+        """Semantische Fehler sollen :class:`RomanConvertException` werfen"""
+        self._semantischer_fehler('IC')
+        self._semantischer_fehler('IIIII')
+        self._semantischer_fehler('VV')
+
+    def _semantischer_fehler(self, roman: str):
+        with self.assertRaisesRegexp(RomanConvertException, 'Eingabe %s beinhaltet semantische Fehler' % roman):
+            roman2decimal(roman)
+
+    def test_fehlende_eingabe(self):
+        """Fehlende Eingabe soll eine :class:`RomanConvertException` erzeugen"""
+        self._fehlende_eingabe('')
+        self._fehlende_eingabe(None)
+
+    def _fehlende_eingabe(self, roman: str):
+        with self.assertRaisesRegexp(RomanConvertException, 'Es wurde keine Eingabe übergeben'):
+            roman2decimal(roman)
+
+    def test_gross_und_kleinschreibung(self):
+        """Warning 'Groß- und Kleinschreibung wurden in der Eingabe %s vermischt' wird erwartet
+
+        Solange alle Buchstaben nur in Klein- oder Großschreibung übertragen werden, soll die Konvertierung normal von
+        statten gehen. Sobald Groß- und Kleinbuchstaben gemischt sind, soll dem Logger 'romanconvertkata' ein Warning
+        übergeben werden.
+        """
+        log = logging.getLogger('romanconvertkata')
+        eingabe = 'mmxx'
+        with self.assertLogs('romanconvertkata', level=logging.WARNING) as cm:
+            self.assertEqual(2020, roman2decimal(eingabe))
+            log.warning('dummy')
+        self.assertEqual(cm.output, ['WARNING:romanconvertkata:dummy'],
+                         'Es darf kein Warning bei mmxx geschrieben werden')
+        eingabe = 'MMXX'
+        with self.assertLogs('romanconvertkata', level=logging.WARNING) as cm:
+            self.assertEqual(2020, roman2decimal(eingabe))
+            log.warning('dummy')
+        self.assertEqual(cm.output, ['WARNING:romanconvertkata:dummy'],
+                         'Es darf kein Warning bei MMXX geschrieben werden')
+        eingabe = 'mmXX'
+        with self.assertLogs('romanconvertkata', level=logging.WARNING) as cm:
+            self.assertEqual(2020, roman2decimal(eingabe))
+        warning = 'WARNING:romanconvertkata:Groß- und Kleinschreibung wurden in der Eingabe %s vermischt' % eingabe
+        self.assertIn(warning, cm.output, "Die Warnmeldung '%s' wurde nicht im Logger registriert." % warning)
