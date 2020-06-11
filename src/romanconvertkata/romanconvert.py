825ee0bc1201f075f60a1db5575d6103bdd82b3b
Philipp Melzer <philipp.melzer@inverso.de>
2020-06-11 10:34:53 +0200

    Test 4 Erfolgreich

--- src/romanconvertkata/romanconvert.py
+++ src/romanconvertkata/romanconvert.py
@@ -13,19 +13,39 @@ def roman2decimal(roman: str) -> int:
     :return: Dezimalwert der Römischen Zahl
     """
     ergebnis = 0
+    previous = None
     for char in roman:
         if char == "i":
             ergebnis += 1
         if char == "v":
+            if previous == "i":
+                ergebnis += 3
+                continue
             ergebnis += 5
         if char == "l":
+            if previous == "x":
+                ergebnis += 30
+                continue
             ergebnis += 50
         if char == "d":
+            if previous == "c":
+                ergebnis += 300
+                continue
             ergebnis += 500
         if char == "x":
+            if previous == "i":
+                ergebnis += 8
+                continue
             ergebnis += 10
         if char == "c":
+            if previous == "x":
+                ergebnis += 80
+                continue
             ergebnis += 100
         if char == "m":
+            if previous == "c":
+                ergebnis += 800
+                continue
             ergebnis += 1000
+        previous = char
     return ergebnis
