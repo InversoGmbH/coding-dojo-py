dabca7b448b251f8c26c5974fd88e32fc9e761b6
Andreas Neumann <andreas.neumann@inverso.de>
2020-06-11 10:09:49 +0200

    test_zehner_potenzen

--- test/romanconvert/test_romanconvert.py
+++ test/romanconvert/test_romanconvert.py
@@ -18,3 +18,15 @@ class TestRomanConvert(TestCase):
         self.assertEqual(5, roman2decimal('v'))
         self.assertEqual(50, roman2decimal('l'))
         self.assertEqual(500, roman2decimal('d'))
+
+    def test_zehner_potenzen(self):
+        """x, xx, xxx, c, cc, ccc, m, mm und mmm als Eingabeparameter"""
+        self.assertEqual(10, roman2decimal('x'))
+        self.assertEqual(20, roman2decimal('xx'))
+        self.assertEqual(30, roman2decimal('xxx'))
+        self.assertEqual(100, roman2decimal('c'))
+        self.assertEqual(200, roman2decimal('cc'))
+        self.assertEqual(300, roman2decimal('ccc'))
+        self.assertEqual(1000, roman2decimal('m'))
+        self.assertEqual(2000, roman2decimal('mm'))
+        self.assertEqual(3000, roman2decimal('mmm'))
