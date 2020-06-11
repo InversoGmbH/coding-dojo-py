8fc456274df5461be04680ad5e2a2a072e64fea7
Andreas Neumann <andreas.neumann@inverso.de>
2020-06-11 10:00:51 +0200

    test_fuenfer_potenzen

--- test/romanconvert/test_romanconvert.py
+++ test/romanconvert/test_romanconvert.py
@@ -12,3 +12,9 @@ class TestRomanConvert(TestCase):
         self.assertEqual(1, roman2decimal('i'))
         self.assertEqual(2, roman2decimal('ii'))
         self.assertEqual(3, roman2decimal('iii'))
+
+    def test_fuenfer_potenzen(self):
+        """v, l, d als Eingabeparameter"""
+        self.assertEqual(5, roman2decimal('v'))
+        self.assertEqual(50, roman2decimal('l'))
+        self.assertEqual(500, roman2decimal('d'))
