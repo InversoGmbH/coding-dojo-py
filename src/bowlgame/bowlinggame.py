3ad59f45e1bbd0d74ea7d7e79e0e80794fea00cd
Kerstin Löber <kerstin.loeber@inverso.de>
2020-07-08 09:52:56 +0200

    BowlingGame 1.

--- src/bowlgame/bowlinggame.py
+++ src/bowlgame/bowlinggame.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from bowlgame.bowlingframe import BowlingFrame
 
 
 class BowlingGame:
     """Klasse für ein Bowlingspiel, welches aus 10 BowlingFrames besteht."""
+    def get_score(self):
+        return 0
+
+    def is_frame(self):
+        return 1
+
+    def get_frame(self):
+        return BowlingFrame()
+
