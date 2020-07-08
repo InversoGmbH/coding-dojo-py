01fa2da9896fcb9d987318f6f82a2a7055dd8e46
Philipp Melzer <philipp.melzer@inverso.de>
2020-07-08 11:21:56 +0200

    KegelGame eingefügt

--- src/bowlgame/kegelframe.py
+++ src/bowlgame/kegelframe.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from bowlgame.bowlexception import BowlToManyPins, BowlToManyShots
 
 
 class KegelFrame:
     """Klasse für einen Durchgang beim Kegelspiel"""
+    MAXIMUM_PINS = 9
+
+    def shot(self, s):
+        if s > self.pins:
+            raise BowlToManyPins()
+        if self.is_finished():
+            raise BowlToManyShots()
+        self.shots = self.shots + 1
+        self.pins = self.pins - s
+
+    def is_finished(self):
+        return self.pins == 0 or self.shots >= 2
+
+    def __init__(self):
+        self.shots = 0
+        self.pins = self.MAXIMUM_PINS
--- src/bowlgame/kegelgame.py
+++ src/bowlgame/kegelgame.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from bowlgame.kegelframe import KegelFrame
 
 
 class KegelGame:
     """Klasse für ein Kegelspiel, welches aus KegelFrames besteht."""
+    def __init__(self):
+        self.frame = KegelFrame()
+
+    def get_score(self):
+        return 0
+
+    def is_frame(self):
+        return 1
+
+    def get_frame(self):
+        if self.frame.is_finished():
+            self.frame = KegelFrame()
+        return self.frame
